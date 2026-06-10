fa17acb9a934bf9d0106513abcdf4913ef98e913
Salim B <git@salim.space>
2026-06-10 12:00:41 +0200

    refactor: broaden newsletter item detection
    match on both `Update` and `Newsletter` tags

--- mise-tasks/fetch/latest-articles.sh
+++ mise-tasks/fetch/latest-articles.sh
@@ -6,19 +6,20 @@ set -euo pipefail
 ARTICLE_LINKS="$(
   (
     echo '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/"><channel>'
+    regex='<category>.*\b(Newsletter|Update)\b.*</category>'
     page=1
     found=0
     while [ $found -eq 0 ]; do
       content=$(curl -s -L -A "Mozilla/5.0" "https://www.digitale-gesellschaft.ch/feed/?paged=$page")
       [[ "$content" != *"<item>"* ]] && break
       echo "$content" | sed -n '/<item>/,/<\/item>/p'
-      [[ "$content" =~ category.*Newsletter.*category ]] && found=1
+      [[ $content =~ $regex ]] && found=1
       ((page++))
       [ $page -gt 10 ] && break
     done
     echo '</channel></rss>'
   ) | \
-    xee xpath "string-join(//item[category = 'Newsletter'][1]/preceding-sibling::item[not(category = ('Veranstaltungen'))]/link, ' ')" | \
+    xee xpath "string-join(//item[category = ('Newsletter', 'Update')][1]/preceding-sibling::item[not(category = ('Veranstaltungen'))]/link, ' ')" | \
     tr -d '"'
 )"
 
